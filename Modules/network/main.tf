9ba29ea76703f50518e4c2962fe701419ae889a1
varunaita <varunaita3005@gmail.com>
2026-07-29 11:55:35 +0000

    hi

---
+++ Modules/network/main.tf
@@ -0,0 +1,22 @@
+resource "aws_security_group" "main" {
+    name = "${var.component}-${var.env}"
+
+
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  ingress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  
+}
+}
