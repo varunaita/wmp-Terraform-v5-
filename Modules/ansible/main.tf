5e012d693ba0cea57d9597833e24539376442a69
varunaita <varunaita3005@gmail.com>
2026-07-29 14:18:32 +0000

    l

--- Modules/ansible/main.tf
+++ Modules/ansible/main.tf
@@ -9,7 +9,7 @@ resource "null_resource" "ansible" {
     }
     inline = [
       "sudo labauto ansible",
-      "ansible-pull -i localhost, -U https://github.com/raghudevopsb88/wmp-ansible-v4.git main.yml -e env=${var.env} -e COMPONENT=${var.component}"
+      "ansible-pull -i localhost, -U https://github.com/varunaita/wmp-ansible-v1.git main.yml -e env=${var.env} -e COMPONENT=${var.component}"
     ]
   }
 }
